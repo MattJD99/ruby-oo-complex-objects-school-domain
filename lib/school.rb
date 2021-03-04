3f82257b9d29648256e93f763d0337581d9b5d9f
Matt D <dayclawtel@gmail.com>
2021-03-03 18:41:20 -0800

    commit tree 1e69aaf32c8685d06611ba7ebcc2d9a5a338c92d

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+
+    attr_reader :name, :roster
+
+    def initialize(name, roster = {})
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+       if
+        @roster.keys.include?(grade)
+        @roster[grade].push name
+       else
+        @roster[grade] =[]
+        @roster[grade].push name
+       end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.collect do |grade, nam|
+            @roster[grade] = nam.sort
+        end
+    @roster
+    end
+end
